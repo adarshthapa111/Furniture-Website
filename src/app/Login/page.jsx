700e124e2db8a77fa6719cccb3908d2b668cf379
Adarsh2003Thapa <100429154+adarshthapa111@users.noreply.github.com>
2024-08-18 18:25:33 +0545

    created branch

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -61,9 +61,9 @@ export default function Login() {
     <div className="flex items-center justify-center min-h-screen bg-background">
       <Card className="w-full max-w-md p-6 sm:p-8">
         <Link href="/">
-          <div className="flex justify-center mb-6">
+          {/* <div className="flex justify-center mb-6">
             <h1 className="font-josefin text-xl font-bold">Hamro Furniture</h1>
-          </div>
+          </div> */}
         </Link>
         <form onSubmit={handleSubmit}>
           <CardContent className="space-y-4">
